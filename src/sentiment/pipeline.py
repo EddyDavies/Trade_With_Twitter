c8cbd7d4ba1c92df9d30a88873b621b3d093cf34
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-09-13 23:14:56 +0100

    Slicing Fixed

--- src/sentiment/pipeline.py
+++ src/sentiment/pipeline.py
@@ -4,7 +4,6 @@ from sentiment.inference import get_paths, get_tweets, get_sentiments, save_sent
 from utils import get_date_array, get_month_array, string_to_month_year, last_day_in_month, check_last_day
 
 if __name__ == '__main__':
-    results_folder, source = get_paths(reset=True)
 
     dates_range = ("2017-01-01", "2021-06-20")
     months_range = ["Jan 17", "Jun 21"]
@@ -14,17 +13,18 @@ if __name__ == '__main__':
 
     sentiment_analysis = pipeline("sentiment-analysis")
 
+    results_folder, source = get_paths(reset=True)
+
     for date in dates:
         ids, tweets = get_tweets(date)
 
         percentage_per_chunk = 1
         save_every = 50
-        results = get_sentiments(date, tweets, ids, results_folder,
-                                 sentiment_analysis=sentiment_analysis,
-                                 save_every=save_every,
-                                 percentage_per_chunk=percentage_per_chunk)
+        get_sentiments(date, tweets, ids, results_folder,
+                         sentiment_analysis=sentiment_analysis,
+                         save_every=save_every,
+                         percentage_per_chunk=percentage_per_chunk)
 
-        save_sentiments(ids, tweets, results_folder, date)
         check_last_day(results_folder, date)
 
 
