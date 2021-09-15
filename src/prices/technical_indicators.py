de4857e81bf086b14d1e2d0b661d410fef80d9e0
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-09-15 09:11:29 +0100

    Paramaterise TA

--- src/prices/technical_indicators.py
+++ src/prices/technical_indicators.py
@@ -1,20 +1,27 @@
+import os.path
+
 import pandas as pd
+from pandas import DataFrame
 import pandas_ta as ta
 import yfinance as yf
 import matplotlib as plt
 
 
+from typing import Optional, List, Literal, Dict, Iterable, TypeVar, Tuple, cast, Generator
+import os
+
 if __name__ == '__main__':
 
     df = pd.DataFrame() # Empty DataFrame
+    crypto = "bitcoin"
+    crypto = "ethereum"
 
     # Load data
-    df = pd.read_csv("../data/market-data/bitcoin.csv", sep=",")
-    # OR if you have yfinance installed
-    # df = df.ta.ticker("aapl")
+    data_folder = "../data"
+    market_folder = os.path.join(data_folder, "market-data")
+    source = os.path.join(market_folder, f"{crypto}.csv")
+    df = pd.read_csv(source, sep=",")
 
-    # VWAP requires the DataFrame index to be a DatetimeIndex.
-    # Replace "datetime" with the appropriate column from your DataFrame
     df.set_index(pd.DatetimeIndex(df["date"]), inplace=True)
     df = df[['open_price', 'close_price', 'percent_change_open', 'percent_change_close',
              'volume', 'market_cap']]
@@ -48,6 +55,11 @@ if __name__ == '__main__':
     df.plot(x='date', y='close_price', figsize=(15, 6), linestyle='--', marker='*', markerfacecolor='r', color='y',
             markersize=10)
 
+
+    result = os.path.join(market_folder, f"{crypto}_ta.csv")
+    df.to_csv(result)
+
+
     # plt.xlabel('Date')
     # plt.ylabel('Price')
     plt.show()
@@ -55,4 +67,32 @@ if __name__ == '__main__':
     # Take a peek
     df.tail()
 
-# vv Continue Post Processing vv
+
+# class TechnicalAnalysisUtils:
+#     """
+#     Uses the pandas-ta to create technical indicators
+#     """
+#
+#     def __init__(self):
+#         pass
+#
+#     def apply_ta(self, cryptos: List[str],
+#                                 save_dir: Optional[str] = "../data/market-data",
+#                                 time_period: Optional[TimePeriod] = None) -> List[DataFrame]:
+#
+#         dataframes = [DataFrame(self.market_data_records(crypto, time_period))
+#                       for crypto in cryptos]
+#
+#         if not os.path.exists(save_dir):
+#             os.mkdir(save_dir)
+#
+#         for crypto, df in zip(cryptos, dataframes):
+#             df.to_csv(f'{save_dir}/{crypto}.csv')
+#
+#         return dataframes
+#
+#
+# if __name__ == '__main__':
+#     cmu = CryptoMarketUtils()
+#
+#     cmu.generate_crypto_dataset(["bitcoin", "ethereum"])
