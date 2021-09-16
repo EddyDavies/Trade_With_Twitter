5f0e182e0e0b00d442aa1188f5bb53420ec7dee8
EddyDavies <38349974+EddyDavies@users.noreply.github.com>
2021-09-16 09:40:51 +0100

    Added Saving Callback

--- src/rl/utils/callbacks.py
+++ src/rl/utils/callbacks.py
@@ -1,3 +1,5 @@
+import json
+
 from .logger import Logger
 
 
--- src/trade.py
+++ src/trade.py
@@ -1,6 +1,6 @@
 from rl.environment import Stonks
 from rl.ppo import PPO
-from rl.utils.callbacks import TestOnEpochEndCallback
+from rl.utils.callbacks import TestOnEpochEndCallback, SaveLoggerCallback
 from rl.utils.epochs import Epochs
 from rl.utils.logger import Logger
 
@@ -17,11 +17,14 @@ USE_SENTIMENT = False
 if __name__ == '__main__':
 
     path = "../data/trade/{}.csv"
+    log_path = "../data/trade/{}_log.csv"
     crypto = "bitcoin"
-    date_types = ["_ta_sa_metrics", "_ta_sa",
+    date_types = ["_ta_sa",
+                  # "_ta_sa_metrics",
                   "_sa.csv", "_ta.csv", "_p"]
-    run_type = f"{crypto}{date_types[4]}"
+    run_type = f"{crypto}{date_types[0]}"
     path = path.format(run_type)
+    log_path = log_path.format(run_type)
     print(path)
 
     env = Stonks(
@@ -52,8 +55,9 @@ if __name__ == '__main__':
 
     logger = Logger(plot=f" Trading with {run_type} ↗")
     testing_callback = TestOnEpochEndCallback(testing_env, agent, render=False, action=lambda a: a[0])
+    saving_callback = SaveLoggerCallback(logger, log_path)
 
-    for epoch, episode in Epochs(50, 25, callbacks=[testing_callback]):
+    for epoch, episode in Epochs(50, 25, callbacks=[testing_callback, saving_callback]):
         observation = env.reset()
         done = False
         while not done:
@@ -64,3 +68,4 @@ if __name__ == '__main__':
             agent.remember(observation, action, prob, val, reward, done)
             logger.log(epoch, episode, reward, info["assets"], action, done)
             observation = observation_
+
